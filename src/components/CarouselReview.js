dc85e18f44c57b208ee7419d674657274b0bdb37
yazseyit77 <yazgeldi.seyidov@gmail.com>
2020-05-23 18:56:18 -0700

    review slider updated

--- src/components/CarouselReview.js
+++ src/components/CarouselReview.js
@@ -1,5 +1,4 @@
 import React from "react"
-import Header from "./header"
 import Slider from "react-animated-slider"
 import "react-animated-slider/build/horizontal.css"
 import "normalize.css/normalize.css"
@@ -11,7 +10,7 @@ const content = [
     title: "Vulputate Mollis Ultricies Fermentum Parturient",
     description:
       "Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis.",
-    button: "Read More",
+    // button: "Read More",
     image: "https://i.imgur.com/ZXBtVw7.jpg",
     user: "Luan Gjokaj",
     userProfile: "https://i.imgur.com/JSW6mEk.png",
@@ -20,7 +19,7 @@ const content = [
     title: "Tortor Dapibus Commodo Aenean Quam",
     description:
       "Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui.",
-    button: "Discover",
+    // button: "Discover",
     image: "https://i.imgur.com/DCdBXcq.jpg",
     user: "Erich Behrens",
     userProfile: "https://i.imgur.com/0Clfnu7.png",
@@ -29,16 +28,31 @@ const content = [
     title: "Phasellus volutpat metus",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.",
-    button: "Buy now",
+    // button: "Buy now",
     image: "https://i.imgur.com/DvmN8Hx.jpg",
     user: "Bruno Vizovskyy",
     userProfile: "https://i.imgur.com/4KeKvtH.png",
   },
 ]
 
+const getConfigurableProps = () => ({
+  slider: "slider",
+  previousButton: "previousButton",
+  nextButton: "nextButton",
+  buttonDisabled: "disabled",
+  track: "track",
+  slide: "slide",
+  hidden: "hidden",
+  previous: "previous",
+  current: "current",
+  next: "next",
+  animateIn: "animateIn",
+  animateOut: "animateOut",
+})
+
 const Review = () => (
   <div>
-    <Slider className="slider-wrapper Autoplay">
+    <Slider className="slider-wrapper" {...getConfigurableProps}>
       {content.map((item, index) => (
         <div
           key={index}
@@ -48,7 +62,11 @@ const Review = () => (
           <div className="inner">
             <h1>{item.title}</h1>
             <p>{item.description}</p>
-            <button>{item.button}</button>
+            {/* <button>{item.button}</button> */}
+
+            <p>
+              Posted by <strong>{item.user}</strong>
+            </p>
           </div>
           <section>
             <img src={item.userProfile} alt={item.user} />
