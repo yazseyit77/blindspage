170847b1e2dc471901ff930eb43c4c4f6ea92075
yazseyit77 <yazgeldi.seyidov@gmail.com>
2020-05-29 15:28:40 -0700

    reviews slider modified

--- src/components/CarouselReview.js
+++ src/components/CarouselReview.js
@@ -1,58 +1,43 @@
 import React from "react"
 import Slider from "react-animated-slider"
-import "react-animated-slider/build/horizontal.css"
+import horizontalCss from "react-animated-slider/build/horizontal.css"
 import "normalize.css/normalize.css"
 import "../styles/slider-animations.css"
 import "../styles/styles.css"
 
 const content = [
   {
-    title: "Vulputate Mollis Ultricies Fermentum Parturient",
+    title: "Really happy with the installation",
     description:
-      "Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis.",
+      "Classy Blinds in US, definitely the best. The service is great and fast! All at reasonable prices so what else can you ask for? Really happy with the installation and how it turned out. ",
     // button: "Read More",
-    image: "https://i.imgur.com/ZXBtVw7.jpg",
-    user: "Luan Gjokaj",
-    userProfile: "https://i.imgur.com/JSW6mEk.png",
+    image:
+      "https://images.unsplash.com/photo-1528502043423-9149c2023296?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1049&q=80",
+    user: "Rita Bennet",
   },
   {
-    title: "Tortor Dapibus Commodo Aenean Quam",
+    title: "They are beautiful, unique, and operate perfectly",
     description:
-      "Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui.",
+      "We loved the Zebra blinds so much that we had them installed throughout our home. They are beautiful, unique, and operate perfectly. The customer service was excellent. Frank and his team couldn't have been better!!",
     // button: "Discover",
-    image: "https://i.imgur.com/DCdBXcq.jpg",
-    user: "Erich Behrens",
-    userProfile: "https://i.imgur.com/0Clfnu7.png",
+    image:
+      "https://images.unsplash.com/photo-1520046345720-d62f2019926b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+    user: "Lanice Lyon",
   },
   {
-    title: "Phasellus volutpat metus",
+    title: "They look sleek and modern",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula.",
+      "They look sleek and modern. Beautiful woven fabric is low maintenance with the option of sheer to full-coverage.  Very versatile and a world better than blinds. So much more affordable, I couldn’t believe it. Also the mechanisms is sturdy and easy to use. My whole house was done in a couple of hours. Straight from Europe and custom made.",
     // button: "Buy now",
-    image: "https://i.imgur.com/DvmN8Hx.jpg",
-    user: "Bruno Vizovskyy",
-    userProfile: "https://i.imgur.com/4KeKvtH.png",
+    image:
+      "https://images.unsplash.com/photo-1534304210138-25219c7d0544?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1189&q=80",
+    user: "Laura Enriquez",
   },
 ]
 
-const getConfigurableProps = () => ({
-  slider: "slider",
-  previousButton: "previousButton",
-  nextButton: "nextButton",
-  buttonDisabled: "disabled",
-  track: "track",
-  slide: "slide",
-  hidden: "hidden",
-  previous: "previous",
-  current: "current",
-  next: "next",
-  animateIn: "animateIn",
-  animateOut: "animateOut",
-})
-
 const Review = () => (
   <div>
-    <Slider className="slider-wrapper" {...getConfigurableProps}>
+    <Slider autoplay={2000} classNames={horizontalCss}>
       {content.map((item, index) => (
         <div
           key={index}
@@ -62,18 +47,10 @@ const Review = () => (
           <div className="inner">
             <h1>{item.title}</h1>
             <p>{item.description}</p>
-            {/* <button>{item.button}</button> */}
-
             <p>
               Posted by <strong>{item.user}</strong>
             </p>
           </div>
-          <section>
-            <img src={item.userProfile} alt={item.user} />
-            <span>
-              Posted by <strong>{item.user}</strong>
-            </span>
-          </section>
         </div>
       ))}
     </Slider>
