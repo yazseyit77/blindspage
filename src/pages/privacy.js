27c047de5e06bcc420b6f8d3ef35151d25c349cb
yazseyit77 <yazgeldi.seyidov@gmail.com>
2020-06-16 09:00:23 -0700

    updated Privacy Policy text

--- src/pages/privacy.js
+++ src/pages/privacy.js
@@ -6,27 +6,182 @@ class PrivacyPolicy extends React.Component {
       <div className="flex flex-col">
         <div className="max-w-fw flex flex-col">
           <div className="pt-10">
-            <h1 className="text-5xl font-light text-center">Privacy Policy</h1>
+            <h1 className="text-5xl font-light text-center">
+              <strong>Privacy Policy</strong>
+            </h1>
+            <h1 className="text-2xl font-light text-center">
+              <strong>Terms and Conditions of Online Payments</strong>
+            </h1>
+            <br />
           </div>
-          <p className="text-center">
-            MerAk Blinds is a Bellevue based company which provides a variety of
-            high-quality blinds for a reasonable price. We provide a
-            consultation to choose you the best type, color of blinds with
-            affordable pricing. Our goal is to become a leader in providing the
-            European quality blinds and installation in US market. When you
-            purchase any of our custom-made products from our website, you can
-            feel confident your product is backed by our mer-ak.com 30-Day Money
-            Back Guarantee. All our blinds are made to specification from the
-            best materials available and ships in 2 weeks. We are here to
-            provide the best customer service possible. If you are not
-            completely satisfied, contact our customer service department
-            immediately and we will make every effort to correct or repair the
-            issue, in order to make your online purchase experience a pleasant
-            one at mer-ak.com.
-          </p>
+          <div className="px-32" style={{ textAlign: "justify" }}>
+            <p>
+              Protecting your private information is our priority. This
+              Statement of Privacy applies to mer-ak.com and MerAk, LLC and
+              governs data collection and usage. For the purposes of this
+              Privacy Policy, unless otherwise noted, all references to MerAk,
+              LLC include mer-ak.com. The MerAk website is an eCommerce site. By
+              using the MerAk website, you consent to the data practices
+              described in this statement.{" "}
+            </p>
+            <p>
+              <strong> Collection of your Personal Information </strong>
+            </p>
+            <p>
+              If you purchase MerAk's products and services, we collect billing
+              and credit card information. This information is used to complete
+              the purchase transaction. If you use to make a payment on our
+              website then you will be redirected to third-party payment
+              processor’s website to complete your payment. MerAk does not
+              collect any of your payment related information
+            </p>
+            <p>
+              We do not collect any personal information about you unless you
+              voluntarily provide it to us. However, you may be required to
+              provide certain personal information to us when you elect to use
+              certain products or services available on the Site. These may
+              include: (a) registering for an account on our Site; (b) entering
+              a sweepstakes or contest sponsored by us or one of our partners;
+              (c) signing up for special offers from selected third parties; (d)
+              sending us an email message; (e) submitting your credit card or
+              other payment information when ordering and purchasing products
+              and services on our Site. To wit, we will use your information
+              for, but not limited to, communicating with you in relation to
+              services and/or products you have requested from us. We also may
+              gather additional personal or non-personal information in the
+              future.
+            </p>
+            <p>
+              <strong>Use of your Personal Information</strong>
+            </p>
+            <p>
+              MerAk collects and uses your personal information to operate its
+              website(s) and deliver the services you have requested. MerAk may
+              also use your personally identifiable information to inform you of
+              other products or services available from MerAk and its
+              affiliates.
+            </p>
+            <p>
+              <strong>Sharing Information with Third Parties </strong>
+            </p>
+            <p>
+              MerAk does not sell, rent or lease its customer lists to third
+              parties. MerAk may share data with trusted partners to help
+              perform statistical analysis, send you email or postal mail,
+              provide customer support, or arrange for deliveries. All such
+              third parties are prohibited from using your personal information
+              except to provide these services to MerAk, and they are required
+              to maintain the confidentiality of your information.
+            </p>
+            <p>
+              MerAk may disclose your personal information, without notice, if
+              required to do so by law or in the good faith belief that such
+              action is necessary to: (a) conform to the edicts of the law or
+              comply with legal process served on MerAk or the site; (b) protect
+              and defend the rights or property of MerAk; and/or (c) act under
+              exigent circumstances to protect the personal safety of users of
+              MerAk, or the public.
+            </p>
+            <p>
+              <strong>Automatically Collected Information</strong>
+            </p>
+            <p>
+              Information about your computer hardware and software may be
+              automatically collected by MerAk. This information can include:
+              your IP address, browser type, domain names, access times and
+              referring website addresses. This information is used for the
+              operation of the service, to maintain quality of the service, and
+              to provide general statistics regarding use of the MerAk website.
+            </p>
+            <p>
+              <strong>Links</strong>
+            </p>
+            <p>
+              This website contains links to other sites. Please be aware that
+              we are not responsible for the content or privacy practices of
+              such other sites. We encourage our users to be aware when they
+              leave our site and to read the privacy statements of any other
+              site that collects personally identifiable information.
+            </p>
+            <p>
+              <strong>Security of your Personal Information</strong>
+            </p>
+            <p>
+              MerAk secures your personal information from unauthorized access,
+              use, or disclosure.
+            </p>
+            <hr />
+            <p>MerAk uses the following methods for this purpose:</p>
+            <p>
+              <strong>TLS Protocol </strong>
+            </p>
+            <p>
+              When personal information (such as a credit card number) is
+              transmitted to other websites, it is protected through the use of
+              encryption, such as the Transport Layer Security (TLS) protocol.
+              We strive to take appropriate security measures to protect against
+              unauthorized access to or alteration of your personal information.
+              Unfortunately, no data transmission over the Internet or any
+              wireless network can be guaranteed to be 100% secure. As a result,
+              while we strive to protect your personal information, you
+              acknowledge that: (a) there are security and privacy limitations
+              inherent to the Internet which are beyond our control; and (b)
+              security, integrity, and privacy of any and all information and
+              data exchanged between you and us through this Site cannot be
+              guaranteed.
+            </p>
+            <p>
+              <strong>E-mail Communications</strong>
+            </p>
+            <p>
+              From time to time, MerAk may contact you via email for the purpose
+              of providing announcements, promotional offers, alerts,
+              confirmations, surveys, and/or other general communication.
+            </p>
+            <p>
+              <strong>External Data Storage Sites</strong>
+            </p>
+            <p>
+              We host our website and store content on servers provided by third
+              party hosting vendors with whom we have contracted.
+            </p>
+            <p>
+              <strong>Changes to this Statement</strong>
+            </p>
+            <p>
+              MerAk reserves the right to change this Privacy Policy from time
+              to time. We will notify you about significant changes in the way
+              we treat personal information by sending a notice to the primary
+              email address specified in your account, by placing a prominent
+              notice on our site, and/or by updating any privacy information on
+              this page. Your continued use of the Site and/or Services
+              available through this Site after such modifications will
+              constitute your: (a) acknowledgment of the modified Privacy
+              Policy; and (b) agreement to abide and be bound by that Policy.
+            </p>
+            <p>
+              <strong>ONLINE PAYMENTS TERMS AND CONDITIONS</strong>
+            </p>
+            <p>
+              MerAk provides online payment options for customers’ convenience.
+              WE ALSO REQUIRE AN EMAIL ADDRESS FOR CONSUMER COMMUNICATION. IF
+              FOR ANY REASON CONSUMERS REVERSE A PAYMENT THROUGH THEIR FINANCIAL
+              INSTITUTION, MERAK WILL ASSUME THE PAYMENT TO BE NULL AND VOID.
+              CUSTOMERS SHOULD CONTACT MERAK COMPANY IN A TIMELY MANNER TO
+              CLARIFY AND RESOLVE ANY PAYMENT RELATED ISSUES. Contact
+              Information MerAk welcomes your questions or comments regarding
+              this Statement of Privacy. If you believe that MerAk has not
+              adhered to this Statement, please contact MerAk at: MerAk, LLC
+              3040 118th Ave SE, Bellevue, WA 98005 Email Address:
+              info@mer-ak.com Telephone number: 123.456.7890 Effective as of
+              June 16, 2020
+            </p>
+          </div>
           <br />
-          {/* <div className="pt-10">
-            <h1 className="text-5xl font-light text-center">Contact Us</h1>
+          <div className="pt-10">
+            <h1 className="text-5xl font-light text-center">
+              <strong>Contact Information</strong>
+            </h1>
           </div>
           <address class="address">
             <ul>
@@ -43,7 +198,10 @@ class PrivacyPolicy extends React.Component {
                 Email: <strong className="strong">info@mer-ak.com</strong>
               </li>
             </ul>
-          </address> */}
+          </address>
+          <div style={{ textAlign: "right", paddingTop: "30px" }}>
+            <h4>Effective as of June 16, 2020</h4>
+          </div>
         </div>
       </div>
     )
